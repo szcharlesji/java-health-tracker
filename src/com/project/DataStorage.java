4794005f651a261bbe571eba63c1e8a714e149a7
CharlieGai <szcharlesji@gmail.com>
2021-12-11 17:57:01 -0500

    add DataStorage

--- src/com/project/DataStorage.java
+++ src/com/project/DataStorage.java
@@ -1,13 +1,41 @@
 package com.project;
 
+import java.io.*;
+
 public class DataStorage {
     public static void saveRecord(Person person) {
-//        double totalSleepTime = person.getTotalSleepTime();
-//        double totalExerciseTime = person.getTotalExerciseTime();
+        try {
+            FileOutputStream f = new FileOutputStream(new File(person.getName() + ".txt"));
+            ObjectOutputStream o = new ObjectOutputStream(f);
 
+            // Write objects to file
+            o.writeObject(person);
+
+            o.close();
+            f.close();
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        } catch (IOException e) {
+            System.out.println("Error initializing stream");
+        }
     }
 
-    public static void loadRecord(String name) {
+    public static Person loadRecord(String name) {
+        try {
+            FileInputStream fi = new FileInputStream(new File(name + ".txt"));
+            ObjectInputStream oi = new ObjectInputStream(fi);
 
+            return (Person) oi.readObject();
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        } catch (IOException e) {
+            System.out.println("Error initializing stream");
+        } catch (ClassNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        return null;
     }
 }
